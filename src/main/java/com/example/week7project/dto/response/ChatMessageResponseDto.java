d59ef5ecbb6395783e840c2356630a3e87954d7e
limeKIWI <pop78978@gmail.com>
2022-09-12 21:35:56 +0900

    #3 dto들 구현

---
+++ src/main/java/com/example/week7project/dto/response/ChatMessageResponseDto.java
@@ -0,0 +1,15 @@
+package com.example.week7project.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class ChatMessageResponseDto {
+    private Long roomId;
+    private Long postId;
+    private String sender;
+    private String message;
+}
