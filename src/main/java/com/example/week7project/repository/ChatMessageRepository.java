e4e7f4c2b8e5146f904619c79a7cdd264eef1918
limeKIWI <pop78978@gmail.com>
2022-09-13 23:12:17 +0900

    #56 채팅방 메세지 조회 jpa 구현

--- src/main/java/com/example/week7project/repository/ChatMessageRepository.java
+++ src/main/java/com/example/week7project/repository/ChatMessageRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-    ChatMessage findByChatRoom(ChatRoom chatRoom);
+    List<ChatMessage> findAllByChatRoomOrderByCreatedAtDesc(ChatRoom chatRoom);
     List<ChatMessage> findByChatRoomOrderByCreatedAtDesc(ChatRoom chatRoom);
 }
